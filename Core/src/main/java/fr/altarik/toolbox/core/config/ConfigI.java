b88d7f3e08692d3bb8d5ae7f9454afc17075c2f1
Quentin Legot <legotquentin@gmail.com>
2024-02-12 19:56:28 +0100

    ConfigI#load now return given clazz in parameter, dump to 5.0.0 because of breaking changes

--- Core/src/main/java/fr/altarik/toolbox/core/config/ConfigI.java
+++ Core/src/main/java/fr/altarik/toolbox/core/config/ConfigI.java
@@ -24,10 +24,10 @@ public class ConfigI {
         return configPath.resolve(name);
     }
 
-    public static ConfigI load(Path configPath, String name, Class<? extends ConfigI> clazz) throws IOException, JsonSyntaxException, JsonIOException {
+    public static <T extends ConfigI> T load(Path configPath, String name, Class<T> clazz) throws IOException, JsonSyntaxException, JsonIOException {
         Path path = getConfigPath(configPath, name);
 
-        ConfigI file;
+        T file;
 
         if(Files.exists(path)) {
             FileReader reader = new FileReader(path.toFile());
